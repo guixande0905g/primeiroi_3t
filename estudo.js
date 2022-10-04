0df268a6e564efce250eee007bf740eb12a5d729
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2022-10-04 17:42:39 -0300

    Aula 04/10

--- estudo.js
+++ estudo.js
@@ -14,5 +14,21 @@ document.write( tabuada + " x 7 = " + (tabuada * 7)+ "<br>")
 document.write( tabuada + " x 8 = " + (tabuada * 8)+ "<br>")
 document.write( tabuada + " x 9 = " + (tabuada * 9)+ "<br>")
 document.write( tabuada + " x 10 = " + (tabuada * 10)+ "<br>")
+}
+//for(inicio, validaçao, manutençao)
+function escreva(){
+   for(var i=0; i <=10; i++){
 
+       for(var j = 1; j <=10; j++){
+        document.write(i + " x " + j + " = " + (j*i)+ "<br>")
+       }
+       document.write("<br>");
+   }
+
+}
+
+function quadrado (){
+    for(var i = 2; i < 11; i++){
+    document.write("O quadrado de " + i + " é " + (i+i)+"<br>");
+    }
 }
