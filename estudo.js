92596af6b4424db6292c5351ee707fe2f15a51d9
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2022-10-11 17:47:50 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -40,3 +40,30 @@ function total(){
     let resultado = (val * ((ju/100)+1));
     document.write("O total é de: " + resultado);
 }
+
+function soma(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4);
+    document.getElementById("resultado"). innerHTML = r;
+}
+
+function media(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = (Number(n1) + Number(n2) + Number(n3) + Number(n4))/3;
+    document.getElementById("resultado").innerHTML = r;
+}
+function falta(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = 180 - (Number(n1) + Number(n2) + Number(n3) + Number(n4));
+    document.getElementById("resultado").innerHTML = r;
+}
+
